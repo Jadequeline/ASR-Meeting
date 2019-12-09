8dbadc44a9efebdc5d4e08a67f711f2a61495127
fikrihasani <mfikrihasani@gmail.com>
2019-12-09 18:48:15 +0700

    Updated with new corpus

---
+++ 2_build_LM.sh
@@ -0,0 +1,2 @@
+htk/LBuild -c 2 0 -c 3 0 -n 2 gram/bigram/wmap gram/bigram/langmodel gram/bigram/gram.*
+htk/LBuild -c 2 0 -c 3 0 -n 3 gram/trigram/wmap gram/trigram/langmodel gram/trigram/gram.*
---
+++ 3_test_LM.sh
@@ -0,0 +1,2 @@
+htk/LPlex -t gram/trigram/langmodel corpus_s1_dan_s2.txt | tee result_model_tri.txt
+htk/LPlex -t gram/bigram/langmodel corpus_s1_dan_s2.txt | tee result_model_bi.txt
